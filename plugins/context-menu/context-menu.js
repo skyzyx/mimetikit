1d93fd03b8dd75bf310182c8dc0a7a2bf62ebeeb
Ryan Parman <ryan@ryanparman.com>
2010-12-18 10:04:26 -0800

    Wrapped up the context menu. Polished the buttons. Bundled jQuery and jQuery UI.

--- plugins/context-menu/context-menu.js
+++ plugins/context-menu/context-menu.js
@@ -15,7 +15,72 @@
 
 
 (function($) {
-	$.fn.contextMenu = function() {
+	var _ = DOMBuilder;
 
+	$.fn.contextMenu = function(buttons) {
+
+		var menu, $menu, $body, underTheMenu;
+
+		// Generate menu
+		document.body.appendChild(_.DOM(
+			menu = _('div', { 'class':'mimetikit-context-container top' }).child([
+				_('div', { 'class':'mimetikit-context-tip-top' }),
+				_('div', { 'class':'mimetikit-context-choices' }).child(
+					_('div', { 'class':'mimetikit-context-choices-visible' }).child(buttons)
+				),
+				_('div', { 'class':'mimetikit-context-tip-bottom' })
+			]).asDOM()
+		));
+
+		// Default positioning
+		$menu = $(menu);
+		$body = $(document);
+		underTheMenu = $(this).offset().top + $(this).outerHeight() + 10;
+
+		$menu.offset({
+			'left': -1000,
+			'top': (underTheMenu - ($menu.outerHeight() * 0.05))
+		});
+
+		// Bind click event
+		$(this).bind('click', function(evt) {
+
+			evt.preventDefault();
+			var callback, clickablePosition, menuPosition;
+
+			clickablePosition = $(this).offset().left + ($(this).width() / 2);
+			menuPosition = clickablePosition - ($menu.width() / 2);
+
+			$menu.offset({
+				'left': Math.floor(menuPosition)
+			});
+
+			$menu.animate({
+				'top': underTheMenu,
+				'opacity': 1
+			}, {
+				'duration': 150,
+				'easing': 'easeOutQuad',
+				'complete': function() {
+					var $cb;
+					$body.click($cb = function(evt) {
+
+						$menu.animate({
+							'top': (underTheMenu - ($menu.outerHeight() * 0.05)),
+							'opacity': 0
+						}, {
+							'duration': 150,
+							'easing': 'easeOutQuad',
+							'complete': function() {
+								$menu.offset({
+									'left': -1000
+								});
+								$body.unbind('click', $cb);
+							}
+						});
+					});
+				}
+			});
+		});
 	};
 })(jQuery);
